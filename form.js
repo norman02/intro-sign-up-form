b270c74111fee687028c01522d730e0e874b6d7b
John N. Norman <normanj777@gmail.com>
2020-07-28 14:08:17 -0400

    add initial validation function

--- form.js
+++ form.js
@@ -3,7 +3,12 @@
 const validate = ()=> {
     const firstName = document.getElementById('first_name').value
     const lastName = document.getElementById('last_name').value
-    console.log('hello' + firstName + lastName)
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    console.log('hello' + ' ' +  firstName + ' ' + lastName + ' at ' + email + ' I hope you realize that ' + password + ' is a stupid password')
 }
 
+
 document.getElementById("submit").addEventListener("click", validate)
+
